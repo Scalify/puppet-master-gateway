7a16a9a753f1da7fd24d8fa409a61656ea9d76dc
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2018-05-18 15:11:47 +0200

    Rename variables field to vars

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -5,14 +5,14 @@ import "encoding/json"
 // A Job is executed by the executor and stored in the database and holds all information
 // required to let the puppets dance in the browser
 type Job struct {
-	ID        string            `json:"id"`
-	Rev       string            `json:"_rev,omitempty"`
-	Code      string            `json:"code"`
-	Status    string            `json:"status"`
-	Variables map[string]string `json:"variables"`
-	Modules   map[string]string `json:"modules"`
-	Error     string            `json:"error,omitempty"`
-	Logs      json.RawMessage   `json:"logs,omitempty"`
+	ID      string            `json:"id"`
+	Rev     string            `json:"_rev,omitempty"`
+	Code    string            `json:"code"`
+	Status  string            `json:"status"`
+	Vars    map[string]string `json:"vars"`
+	Modules map[string]string `json:"modules"`
+	Error   string            `json:"error,omitempty"`
+	Logs    json.RawMessage   `json:"logs,omitempty"`
 }
 
 // A JobResult is emitted after a worker did the job and synced to database
